bf240156221e01d116973ab92c40440f8e80ea83
Rohit Nimbalkar <nimbalkarrohit52@gmail.com>
2026-06-14 22:03:44 +0530

    Hero Section done

---
+++ components/AddProductForm.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Input } from "./ui/input";
+
+const AddProductForm = ({ user }) => {
+  return (
+    <form>
+      <div>
+        <Input />
+      </div>
+    </form>
+  );
+};
+
+export default AddProductForm;
